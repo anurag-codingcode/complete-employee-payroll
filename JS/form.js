17dd23630574dbab7872d8b50320e793673d3c96
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-29 10:45:24 +0530

    implemented backend and frontend of employee payroll dashboard

---
+++ JS/form.js
@@ -0,0 +1,79 @@
+$(document).ready(() => {
+    var daySelect = $("#daySelect")
+    for (let i = 1; i <= 31; i++) {
+        daySelect.append($('<option>', {
+            value: i,
+            text: i,
+            
+        }))
+    }
+    var monthSelect = $('#monthSelect')
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    monthNames.forEach((month)=>{
+        monthSelect.append($('<option>',{
+            text:month,
+            value:month.slice(0,3)
+        }))
+    })
+
+    var yearSelect=$('#yearSelect')
+    var currentYear=new Date().getFullYear()
+
+    for (let i=currentYear;i>=currentYear-100;i--){
+        yearSelect.append($('<option>',{
+            value:i,
+            text:i
+        }))
+
+    }
+
+    $("form").submit(function (e) {
+        e.preventDefault()
+        formSubmit()
+    });
+})
+
+
+function formSubmit() {
+
+    var name = $('form')[0].elements['name'].value
+    var img = $('form')[0].elements['img'].value
+    var gender = $('form')[0].elements['gender'].value
+    var dep = $('form')[0].elements['department']
+    var department = []
+    dep.forEach((element) => {
+        if (element.checked) {
+            department.push(element.value)
+        }
+
+    })
+    var salary = $('form')[0].elements['salary'].value
+    var day=$('form')[0].elements['daySelect'].value
+    var month=$('form')[0].elements['monthSelect'].value
+    var year=$('form')[0].elements['yearSelect'].value
+    console.log(salary)
+    var startDate=`${day} ${month} ${year}`
+
+
+    var notes=$('form')[0].elements['notes'].value
+    if(department.length==0){
+        alert("select all options")
+        return
+    }
+    const toSend=JSON.stringify({
+            name,img,department,salary,startDate,notes,gender
+        })
+    
+    
+    $.ajax({
+        url:"http://localhost:3000/employee",
+        type:'POST',
+        data:toSend,
+        success:(data)=>{
+            alert("successfully added")
+
+        }
+    })
+ 
+
+}
--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,50 @@
 $(document).ready(() => {
-
-    // $.get('http://localhost:3000/employee',(data,status)=>{
-    //     console.log(status)
-    //     console.log(data)
-    // })
+    var tbody = $(tbody)
 
     $.ajax({
-        url:"http://localhost:3000/employee",
-        type:"GET",
-        success:(data)=>{
-            console.log(data)
-            
+        url: "http://localhost:3000/employee",
+        type: "GET",
+        success: (data) => {
+
+            var tbody = $('tbody')[0]
+
+            for (let i = 0; i < data.length; i++) {
+                var { id,img, name, gender, department, salary, startDate } = data[i]
+
+                var trow = $('<tr></tr>');
+                var td1 = $(`<td><img src='${img}' alt=""></td>`);
+                var td2 = $("<td></td>").text(name);
+                var td3 = $("<td></td>").text(gender);
+                var td4 = $(`<td>${department.map(element => `<span class="crl">${element}</span>`).join(' ')}</td>`);
+
+                var td5 = $(`<td>₹${salary}</td>`)
+                var td6 = $('<td></td>').text(startDate)
+                var td7 = $(`<td class="icons"><span><img class="delIcon" id="${id}" src="Assets/delete-black-18dp.svg" height="28px" width="28px" alt=""></span><span><img class="creIcon" src="Assets/create-black-18dp.svg" style="margin-left: 50px;" height="28px" width="28px" alt=""></span></td>`)
+                trow.append(td1, td2, td3, td4, td5, td6, td7)
+
+                tbody.append(trow[0])
+            }
 
         }
-    })
+    }
+    )
+    $('tbody').on('click', '.delIcon', function() {
+        delEmployee(this.id);
+    });
+
+    
+    
 }
 )
+
+function delEmployee(id){
+    $.ajax({
+        url:"http://localhost:3000/employee/"+id,
+        type:"DELETE",
+        success:()=>{
+            alert("deleted successfully")
+        }
+    })
+    
+
+}
