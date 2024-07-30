3cd3e25d5e5cf66fc181230ba77ba3ab662b95d0
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-07-30 17:18:11 +0530

    implemented new features

--- JS/form.js
+++ JS/form.js
@@ -1,45 +1,83 @@
 $(document).ready(() => {
+    if (localStorage.getItem('id')) {
+        getFormElements(localStorage.getItem('id'))
+    }
 
     dateSelect()
-    
+
     $("form").submit(function (e) {
         e.preventDefault()
         formSubmit()
     });
 })
 
-function dateSelect(){
+function getFormElements(id) {
+
+    var x = $.ajax({
+        url: "http://localhost:3000/employee/" + id,
+        type: 'GET',
+        success: (data) => {
+            console.log(data)
+
+            $('#name')[0].value = data.name
+            $(`input[class='form-check-input'][value='${data.img}']`).prop('checked', true)
+            $(`input[class='form-check-input'][value='${data.gender}']`).prop('checked', true)
+            const departments = data.department
+            for (let i = 0; i < departments.length; i++) {
+                $(`input[class='form-check-input'][value='${departments[i]}']`).prop('checked', true)
+            }
+            $(`option[value='${data.salary}']`).prop('selected', true)
+            const [day, month, year] = data.startDate.split(" ")
+            $(`option[value='${day}']`).prop('selected', true)
+            $(`option[value='${month}']`).prop('selected', true)
+            $(`option[value='${year}']`).prop('selected', true)
+            $('textarea')[0].value = data.notes
+        }
+
+    })
+
+
+
+
+
+}
+function dateSelect() {
     var daySelect = $("#daySelect")
     for (let i = 1; i <= 31; i++) {
         daySelect.append($('<option>', {
             value: i,
             text: i,
-            
+
         }))
     }
     var monthSelect = $('#monthSelect')
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    monthNames.forEach((month)=>{
-        monthSelect.append($('<option>',{
-            text:month,
-            value:month.slice(0,3)
+    monthNames.forEach((month) => {
+        monthSelect.append($('<option>', {
+            text: month,
+            value: month.slice(0, 3)
         }))
     })
 
-    var yearSelect=$('#yearSelect')
-    var currentYear=new Date().getFullYear()
+    var yearSelect = $('#yearSelect')
+    var currentYear = new Date().getFullYear()
 
-    for (let i=currentYear;i>=currentYear-100;i--){
-        yearSelect.append($('<option>',{
-            value:i,
-            text:i
+    for (let i = currentYear; i >= currentYear - 100; i--) {
+        yearSelect.append($('<option>', {
+            value: i,
+            text: i
         }))
 
     }
 }
 
 function formSubmit() {
+    let url = "http://localhost:3000/employee"
+    let method = 'POST'
+    const id = localStorage.getItem('id')
 
+    console.log(url)
+    console.log(method)
     var name = $('form')[0].elements['name'].value
     var img = $('form')[0].elements['img'].value
     var gender = $('form')[0].elements['gender'].value
@@ -52,31 +90,35 @@ function formSubmit() {
 
     })
     var salary = $('form')[0].elements['salary'].value
-    var day=$('form')[0].elements['daySelect'].value
-    var month=$('form')[0].elements['monthSelect'].value
-    var year=$('form')[0].elements['yearSelect'].value
-    console.log(salary)
-    var startDate=`${day} ${month} ${year}`
-
-
-    var notes=$('form')[0].elements['notes'].value
-    if(department.length==0){
+    var day = $('form')[0].elements['daySelect'].value
+    var month = $('form')[0].elements['monthSelect'].value
+    var year = $('form')[0].elements['yearSelect'].value
+    var startDate = `${day} ${month} ${year}`
+    var notes = $('form')[0].elements['notes'].value
+    if (department.length == 0) {
         alert("select all options")
         return
     }
-    const toSend=JSON.stringify({
-            name,img,department,salary,startDate,notes,gender
-        })
-    
-    
+    let toSend = {
+        name, img, department, salary, startDate, notes, gender
+    }
+    if (id) {
+        url = "http://localhost:3000/employee/" + id
+        method = 'PUT'
+        console.log(url,method)
+        toSend['id']=id
+        console.log(toSend)
+        localStorage.removeItem('id')
+    }
+    toSend=JSON.stringify(toSend)
     $.ajax({
-        url:"http://localhost:3000/employee",
-        type:'POST',
-        data:toSend,
-        success:(data)=>{
+        url: url,
+        type: method,
+        data: toSend,
+        success: (data) => {
             window.location.replace("index.html");
         }
     })
- 
+
 
 }
--- JS/index.js
+++ JS/index.js
@@ -6,14 +6,8 @@ $(document).ready(() => {
     console.log("dleted value",deleted)
 
 
-    getUser('a')
-
-    $('tbody').on('click', '.delIcon', function () {
-
-        delEmployee(this.id);
-
-    });
-    
+    getUser('')
+ 
 
     $('#search-icon').on('click', () => {
         click = true
@@ -56,16 +50,16 @@ function getUser(filterData) {
         type: "GET",
         success: (data) => {
             console.log(data)
-            if (filterData!=NaN && filterData != 'a') {
+            if (filterData!=NaN && filterData != '') {
                 data = filterItems(data,filterData)
             }
             var tbody = $('tbody')[0]
             tbody.innerHTML = ''
-
+           
             for (let i = 0; i < data.length; i++) {
                 var { id, img, name, gender, department, salary, startDate } = data[i]
 
-                var trow = $('<tr></tr>');
+                var trow = $(`<tr id="${id}"></tr>`);
                 var td1 = $(`<td><img src='${img}' alt=""></td>`);
                 var td2 = $("<td></td>").text(name);
                 var td3 = $("<td class='gender-hide'></td>").text(gender);
@@ -73,7 +67,7 @@ function getUser(filterData) {
 
                 var td5 = $(`<td class="salary-hide">₹${salary}</td>`)
                 var td6 = $('<td  class="startDate-hide"></td>').text(startDate)
-                var td7 = $(`<td class="icons"><span><img class="delIcon" id="${id}" src="Assets/delete-black-18dp.svg" height="28px" width="28px" alt=""></span><span><img class="creIcon" src="Assets/create-black-18dp.svg" style="margin-left: 50px;" height="28px" width="28px" alt=""></span></td>`)
+                var td7 = $(`<td class="icons"><span onclick="delEmployee('${id}')" ><img class="delIcon" src="Assets/delete-black-18dp.svg" height="28px" width="28px" alt=""></span><span onclick="ediEmployee('${id}')" ><img class="creIcon" src="Assets/create-black-18dp.svg" style="margin-left: 50px;" height="28px" width="28px" alt=""></span></td>`)
                 trow.append(td1, td2, td3, td4, td5, td6, td7)
 
                 tbody.append(trow[0])
@@ -105,4 +99,9 @@ function delEmployee(id) {
     })
 
 
+}
+function ediEmployee(id){
+    console.log(id)
+    window.location.replace('form.html')
+    localStorage.setItem("id",id)
 }
